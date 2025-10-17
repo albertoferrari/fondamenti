a0400e83816cc2e729f9638e2a70fd80defdf125
Alberto Ferrari <alberto.ferrari@unipr.it>
2025-10-17 15:09:13 +0200

    Add files via upload

---
+++ materiale/n_quadrati.py
@@ -0,0 +1,32 @@
+'''
+Esempio linearità
+- input n
+- disegnare n quadrati
+- il primo grande come il canvas (500x500)
+- l'ultimo di lato 10
+- tutti centrati con il canvas
+- tutti con colori casuali
+'''
+import g2d
+from random import randrange
+
+g2d.init_canvas((500,500))
+
+'''
+g2d.set_color((randrange(255),randrange(255),randrange(255)))
+g2d.draw_rect((0,0),(500,500))
+
+g2d.set_color((randrange(255),randrange(255),randrange(255)))
+g2d.draw_rect((245,245),(10,10))
+
+n = int(g2d.prompt('n: '))
+
+for i in range(n):
+    g2d.set_color((randrange(255),randrange(255),randrange(255)))
+    
+    pos = 245 / max(n-1,1) * i
+    dim = -490 / max(n-1,1) * i + 500
+    
+    g2d.draw_rect((pos, pos), (dim,dim))
+'''
+g2d.main_loop()
